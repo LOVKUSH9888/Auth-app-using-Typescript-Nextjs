cf7b4d1d581d64a1a97fea8262cbb7fbb9ffc7a9
LOVKUSH9888 <lovkushyadav9888@gmail.com>
2024-02-25 23:12:56 +0530

    worked in the encapsulated the schema logics

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -30,4 +30,38 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+//Hashing the password before saving it into the DB
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+  }
+  next();
+});
+
+userSchema.methods = {
+  comparePassword: async function (loginPassword) {
+    const isMatched = await bcrypt.compare(loginPassword, this.password);
+    return isMatched;
+  },
+
+  generateAuthToken: function () {
+    const user = this;
+    const payLoad = {
+      _id: user._id,
+      email: user.email,
+      password: user.password,
+    };
+
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    return token;
+  },
+};
+
 module.exports = mongoose.model("User", userSchema);
